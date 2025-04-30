baff4c183c0344bff3aa35b150bb7c95d7838d3c
Samuel <samuelgamer820@gmail.com>
2025-04-30 18:01:29 -0500

    Map is now full page

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -16,13 +16,13 @@ class _MapPageState extends State<MapPage> {
   //Set the initial pos to somewhere on the image
   void initState() {
     super.initState();
-    _transformationController.value = Matrix4.identity()
-    ..setTranslationRaw(-1300, -600, 0);
+    _transformationController.value =
+        Matrix4.identity()..setTranslationRaw(-1200, -430, 0);
   }
 
   void _handleTap(BuildContext context, TapUpDetails details) {
     //Convert global tap position to local within the InteractiveViewer
-    final offset = Offset(33.5, 155);
+    final offset = Offset(14, 27);
     final renderBox = context.findRenderObject() as RenderBox;
     final localPosition = renderBox.globalToLocal(details.globalPosition);
 
@@ -47,15 +47,21 @@ class _MapPageState extends State<MapPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        body: Stack(
           children: [
-            _welcomeText('Moises'), 
-            _mapVisualizer()
+            _mapVisualizer(),
+            Padding(
+              padding: const EdgeInsets.only(top: 40, left: 20, right: 20),
+              child: Container(
+                height: 100,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(18),
+                  color: Colors.amber
+                ),
+              ),
+            ),
             ],
         ),
-      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => _resetMarkers(),
       ),
@@ -64,60 +70,33 @@ class _MapPageState extends State<MapPage> {
 
   Expanded _mapVisualizer() {
     return Expanded(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(20),
-          child: GestureDetector(
-            onTapUp: (details) {
-              _handleTap(context, details);
-              print(defaultMarkers);
-              },
-            child: InteractiveViewer(
-              transformationController: _transformationController,
-              boundaryMargin: EdgeInsets.all(8.0),
-              constrained: false,
-              child: Stack(
-                children: [
-                  SizedBox(
-                    height: 1500,
-                    child: Image.asset('assets/unimap.png', fit: BoxFit.cover),
-                  ),
-                    //Add markers and stuff
-                  ...defaultMarkers.map(
-                    (offSet) => Positioned(
-                      left: offSet.dx,
-                      top: offSet.dy,
-                      child: Icon(
-                        Icons.location_on,
-                        color: Colors.red,
-                        size: 30,
-                      ),
-                    ),
-                  ),
-                ],
+      child: GestureDetector(
+        onTapUp: (details) {
+          _handleTap(context, details);
+          print(defaultMarkers);
+        },
+        child: InteractiveViewer(
+          transformationController: _transformationController,
+          boundaryMargin: EdgeInsets.all(8.0),
+          constrained: false,
+          child: Stack(
+            children: [
+              SizedBox(
+                height: 1500,
+                child: Image.asset('assets/unimap.png', fit: BoxFit.cover),
               ),
-            ),
+              //Add markers and stuff
+              ...defaultMarkers.map(
+                (offSet) => Positioned(
+                  left: offSet.dx,
+                  top: offSet.dy,
+                  child: Icon(Icons.location_on, color: Colors.red, size: 30),
+                ),
+              ),
+            ],
           ),
         ),
       ),
     );
   }
-}
-
-Padding _welcomeText(String name) {
-  return Padding(
-    padding: const EdgeInsets.only(left: 20, top: 20),
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          'Welcome,',
-          //TODO: tune style
-          style: TextStyle(fontSize: 24),
-        ),
-        Text(name, style: TextStyle(fontSize: 20)),
-      ],
-    ),
-  );
 }
